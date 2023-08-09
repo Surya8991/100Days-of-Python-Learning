20b54cc057c629c424422803a25259db3fcd72b4
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-09 11:59:46 +0530

    Day7

--- Day7 Hangman/Day7 Hangman.py
+++ Day7 Hangman/Day7 Hangman.py
@@ -4,16 +4,12 @@ import random
 # import modules from another files
 from Hangman_words import word_list
 from Hangman_art import logo, stages
-
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py
-# Delete this line: word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
 end_of_game = False
 lives = 6
 
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
 print(logo)
 # Testing code
 print(f'Pssst, the solution is {chosen_word}.')
@@ -26,7 +22,6 @@ for _ in range(word_length):
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
 
     # Check guessed letter
     for position in range(word_length):
