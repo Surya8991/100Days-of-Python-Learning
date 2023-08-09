dd9bc850e54fcf2bf39e5ef5f596ff48e9006fe9
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-09 22:28:20 +0530

    Added Invalid method if direction is neither encode or decode

--- Day8/Day8 Ceaser Cipher/Day8 Ceaser Cipher.py
+++ Day8/Day8 Ceaser Cipher/Day8 Ceaser Cipher.py
@@ -22,6 +22,9 @@ def ceaser_cipher(enc_dec,plain_text,shift_amount):
 should_end=False
 while not should_end:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction != "encode" and direction != "decode":
+        print("Invalid Method")
+        break
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
     shift=shift%26
