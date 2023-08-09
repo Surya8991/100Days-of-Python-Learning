d9097bdd5d42893aa7123e23f100a38bd5f2cc37
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-09 11:46:42 +0530

    Day1

---
+++ Day1/Day1 Print Your Band Name.py
@@ -0,0 +1,5 @@
+# Learning Concepts(input , print)
+print("Welcome to Band Name Genertaor 😎")
+city=input("Enter Your City Name \n")
+animal=input("Enter Your Pet's Name or a Name you Like \n")
+print(f"Thank You for using Brand Generator Your Band Name is \n {city} {animal}")
