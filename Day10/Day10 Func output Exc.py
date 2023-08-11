181e1e6e0f6065c80156f700ae320400e12cc784
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-11 15:39:43 +0530

    Day10 done

---
+++ Day10/Day10 Func output Exc.py
@@ -0,0 +1,6 @@
+# Program to learn about functions with outputs
+def format_name(f_name,l_name):
+    name=f_name+" "+l_name
+    formatted_name=name.title()
+    return formatted_name
+print(format_name("rama","raju"))
