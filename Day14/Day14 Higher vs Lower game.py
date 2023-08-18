8d4704c4ca5e5f07bb5e92457a279047e2c0ef34
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-18 20:55:55 +0530

    Day14 Need to fix the code where celeb are generated random

--- Day14/Day14 Higher vs Lower game.py
+++ Day14/Day14 Higher vs Lower game.py
@@ -3,13 +3,17 @@ import random
 from art import logo, vs
 from celeb_data import data
 
-celebA=random.choice(data)
-celebB=random.choice(data)
-score=0
-print("CelebA :- ",celebA["name"],celebA["follower_count"],celebA["description"])
-print("CelebB :- ",celebB["name"],celebB["follower_count"],celebB["description"])
 
-celeb_choice=input("Who has more followers ? Type 'A' or 'B' ")
+score = 0
+def random_data():
+   return random.choice(data)
+
+celebA = random_data()
+celebB = random_data()
+print("CelebA :- ", celebA["name"],
+      celebA["follower_count"], celebA["description"])
+print("CelebB :- ", celebB["name"],
+      celebB["follower_count"], celebB["description"])
 
 # def celeb_follower_comp():
 #     if celebA["follower_count"]>celebB["follower_count"]:
@@ -20,21 +24,38 @@ celeb_choice=input("Who has more followers ? Type 'A' or 'B' ")
 #         print(celebB["name"],f"Wins with follwer count of {winner_count} Millons")
 #     elif celebA["follower_count"] == celebB["follower_count"]:
 #         print("Draw as both as same follower count")
-        
+
 # celeb_follower_comp()
 
+
 def celeb_follower_compare():
-     if celebA["follower_count"]>celebB["follower_count"]:
+    if celebA["follower_count"] > celebB["follower_count"]:
         return "A"
-     elif celebA["follower_count"] < celebB["follower_count"]:
+    elif celebA["follower_count"] < celebB["follower_count"]:
         return "B"
-     elif celebA["follower_count"] == celebB["follower_count"]:
+    elif celebA["follower_count"] == celebB["follower_count"]:
         return "AB"
 
-celeb_follower_winner=celeb_follower_compare()
+
+celeb_follower_winner = celeb_follower_compare()
 
 print(celeb_follower_winner)
 
+
+
 def celeb_add_score():
-    if celeb_follower_winner == celeb_choice:
-        print("Your guess is correct")
+    global score  # Declare score as global
+    game_over = False
+    while not game_over:
+        celeb_choice = input("Who has more followers ? Type 'A' or 'B' ").upper()
+        if celeb_follower_winner == celeb_choice:
+            score += 1
+            print(f"Your guess is correct Your Score :- {score}")
+        else:
+            game_over=True
+            print(f"Sorry Wrong Answer! Your Final Score :- {score} ")
+            
+def high_low_game():
+   celeb_add_score()
+   
+high_low_game()
