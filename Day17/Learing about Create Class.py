4b121ff172d34b3a9be8e2cbc631bb324d47576a
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-24 07:12:20 +0530

    Day17 Learned to Create Custom Class , Add attributes and Methods

---
+++ Day17/Learing about Create Class.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name, classroom,favSubject):
+        self.name=name
+        self.classroom=classroom
+        self.favSubject=favSubject
+
+student1=Student("Ram","10th","English")
+
+print(student1.name)
