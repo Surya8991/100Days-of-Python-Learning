8ad961e12d6fd8d1d29e948c2e69c7994787bc24
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-30 13:02:31 +0530

    5th chal done

---
+++ Day18/Turtle Art/challenge5.py
@@ -0,0 +1,31 @@
+
+import turtle as t
+from turtle import Screen,colormode
+import random
+
+tim = t.Turtle()
+tim.speed(speed=100)
+
+# To generate Random colors
+t.colormode(255)
+def rand_color():
+    r=random.randint(0,255)
+    g=random.randint(0,255)
+    b=random.randint(0,255)
+    color=(r,g,b)
+    return color
+
+# To draw cirlce
+def draw_spirograpph(num_sides):
+    for _ in range(num_sides):
+        tim.circle(100)
+        tim.right(10)
+
+# To specify no. of circles
+for shape_no in range(13):
+    draw_spirograpph(shape_no)
+    tim.color(rand_color())
+
+
+scr=Screen()
+scr.exitonclick()
