c1c584683893cc62115d4a73f20d14544058f087
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-08-30 12:21:37 +0530

    challenge 4 compelted

---
+++ Day18/Turtle Art/Challenge4 random Walk.py
@@ -0,0 +1,23 @@
+# Draw different type of shape
+
+from turtle import Turtle, Screen,colormode
+import random
+
+tim = Turtle()
+
+tim.speed(speed=100)
+tim.pensize(15)
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+direction=[0,90,180,270,360]
+
+def random_walk(num_walks):
+    for _ in range(num_walks):
+        tim.forward(30)
+        tim.right(random.choice(direction))
+
+for walk_tim in range(3, 300):
+    random_walk(walk_tim)
+    tim.color(random.choices(colours))
+
+scr = Screen()
+scr.exitonclick()
