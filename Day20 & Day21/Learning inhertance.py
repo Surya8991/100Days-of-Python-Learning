7219b680a525938c9ecb06d8fef39d2c4f731d33
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-09-06 22:33:50 +0530

    Learning inheritance

---
+++ Day20 & Day21/Learning inhertance.py
@@ -0,0 +1,17 @@
+class Animal():
+    def __int__(self):
+        num_eyes=2
+    def breathe(self):
+        print("Inhale , Exhale")
+
+class Fish(Animal):
+    # Animal is the name of class to inhert
+    # Using super().__init() we are inherting the properties of Animal class to Fish Class
+    def __int__(self):
+        super().__int__()
+    def swim(self):
+        print("Moving in water.")
+
+nemo=Fish()
+nemo.swim()
+nemo.breathe()
