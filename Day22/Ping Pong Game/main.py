a7c4c74f6f713f70e25c5f05a5b074a8e6b01adb
SURYA_L <85568108+Surya8991@users.noreply.github.com>
2023-09-11 23:46:34 +0530

    Create a Paddle classes and made the paddles move

--- Day22/Ping Pong Game/main.py
+++ Day22/Ping Pong Game/main.py
@@ -1,7 +1,24 @@
-from turtle import Turtle,Screen
+from turtle import Turtle, Screen
+from paddle import Paddle
 
-scr=Screen()
-scr.setup(width=800,height=600)
+scr = Screen()
+scr.setup(width=800, height=600)
 scr.bgcolor("black")
 scr.title("Ping Pong Game")
+scr.tracer(0)
+
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
+
+scr.listen()
+scr.onkey(r_paddle.go_up, "Up")
+scr.onkey(r_paddle.go_down, "Down")
+scr.onkey(l_paddle.go_up, "w")
+scr.onkey(l_paddle.go_down, "s")
+is_game_on = True
+while is_game_on:
+    scr.update()
+
+
+
 scr.exitonclick()
